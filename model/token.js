975b9dbb5bc8f7e9c8d88a812ea03fadf6376f0a
Andy Richardson <contact@andythedeveloper.com>
2016-08-06 18:50:44 +0100

    Remove unused legacy function

--- model/token.js
+++ model/token.js
@@ -2,22 +2,7 @@ const Promise = require('bluebird');
 const jwt = Promise.promisifyAll(require('jsonwebtoken'), {suffix: 'Prom'});
 const config = require('../private/config');
 
-/* VERIFY TOKEN STATE */
-module.exports.verify = function(req, res, next){
-    return next();
-    const accessToken = (req.headers.accesstoken != undefined) ? req.headers.accesstoken : req.body.token;
-
-    return jwt.verifyProm(accessToken, config.jwt.secret)
-    .then(function(data){
-        req.decoded = data;
-        return next();
-    })
-    .catch(function(err){
-        res.status(403);
-        return res.json(err);
-    });
-};
-
+/* VERIFY AND DECRYPT TOKEN */
 module.exports.decrypt = function(token){
     return new Promise(function(fulfill, reject){
         return jwt.verifyProm(token, config.jwt.secret)
